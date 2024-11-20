440b23bbd6f4ed3ee146ae240ac8f728561f5951
NDMHVLC <nedimhavlaci@gmail.com>
2024-11-20 20:54:36 +0300

    20.11

---
+++ OnlineEdu.WebUI/Validators/BlogCategory/CreateBlogCategoryValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using OnlineEdu.WebUI.DTOs.BlogCategoryDtos;
+
+namespace OnlineEdu.WebUI.Validators.BlogCategory
+{
+	public class CreateBlogCategoryValidator : AbstractValidator<CreateBlogCategoryDto>
+	{
+		public CreateBlogCategoryValidator()
+		{
+			RuleFor(x => x.Name).NotEmpty().WithMessage("Blog category Adı Boş Geçilemez");
+			RuleFor(x => x.Name).MaximumLength(30).WithMessage("Blog category Adı En Fazla 30 Karakter Olmalıdır");
+		}
+	}
+
+}
